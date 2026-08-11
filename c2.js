9b504280bba7a7980073ce004b3374021e0dd1d0
sriachatterjee414-ai <sriachatterjee414@gmail.com>
2026-08-11 23:48:53 +0530

    Create c2.js

---
+++ c2.js
@@ -0,0 +1,5 @@
+window.chapter2Data = [
+    { type: "bg", image: "black", transition: "fade" },
+    { type: "dialogue", speaker: "System", text: "Part 2 content loaded." },
+    { type: "return_to_menu" }
+];
